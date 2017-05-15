6e1fb5c231a46cb72d8afccedf6a8100ad8eecfc
Alexandr Dzhurinskij <adzhurinskij@gmail.com>
2017-05-15 15:06:04 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ setup(
     author_email='adzhurinskij@gmail.com',
     license='MIT',
     name='phpipam-api-pythonclient',
-    version='0.1.1',
+    version='0.1.2',
     url='https://github.com/adzhurinskij/phpipam-api-pythonclient',
     description='Python Library for PHPIPAM API',
     packages=find_packages(),
